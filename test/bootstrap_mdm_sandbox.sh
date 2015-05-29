1cb40e5853b33fcf8e67b9ceea9aacc11d458a34
filebat.mark@gmail.com <filebat.mark@gmail.com>
2015-05-29 00:02:25 -0400

    bootstrap mdm

--- test/bootstrap_mdm_sandbox.sh
+++ test/bootstrap_mdm_sandbox.sh
@@ -5,12 +5,12 @@
 ## Description :
 ## --
 ## Created : <2015-05-28>
-## Updated: Time-stamp: <2015-05-28 13:43:24>
+## Updated: Time-stamp: <2015-05-29 00:01:56>
 ##-------------------------------------------------------------------
 function log() {
     local msg=${1?}
     echo -ne `date +['%Y-%m-%d %H:%M:%S']`" $msg\n"
-    
+
     if [ -n "$LOG_FILE" ]; then
         echo -ne `date +['%Y-%m-%d %H:%M:%S']`" $msg\n" >> $LOG_FILE
     fi
@@ -24,40 +24,108 @@ function ensure_is_root() {
     fi
 }
 
+################################################################################################
 function install_docker() {
     if ! which docker 1>/dev/null 2>/dev/null; then
         log "Install docker: $command"
         command="wget -qO- https://get.docker.com/ | sh"
         eval $command
     else
-        log "docker exists, skip installation"
+        log "docker service exists, skip installation"
     fi
 }
 
+function create_enough_loop_device() {
+    # Docker start may fail, due to "There are no more loopback devices available."
+    for i in {0..20}
+    do
+        if [ ! -b /dev/loop$i ]; then
+            mknod -m0660 /dev/loop$i b 7 $i
+        fi
+    done
+}
+
+function docker_pull_image() {
+    local image_name=${1?}
+    command="docker pull $image_name"
+
+    output=$(eval $command)
+    if echo "$output" | grep "Status: Image is up to date" 1>/dev/null 2>/dev/null; then
+        echo "no"
+    else
+        echo "yes"
+    fi
+}
+
+function is_container_running(){
+    local container_name=${1?}
+    if docker inspect $container_name 1>/dev/null 2>/dev/null; then
+        if docker ps | grep $container_name  1>/dev/null 2>/dev/null; then
+            echo "running"
+        else
+            echo "dead"
+        fi
+    else
+        echo "none"
+    fi
+}
+
+function docker_update_image() {
+    local image_name=${1?}
+    local container_name=${2?}
+
+    log "docker pull $image_name. This steps may take tens of minutes"
+    has_new_version=$(docker_pull_image $image_name)
+    if [ "$has_new_version" = "yes" ]; then
+        if [ $(is_container_running $container_name) = "running" ]; then
+            log "Stop container($container_name), since image($image_name) has a new version"
+            docker stop $container_name
+        fi
+    else
+        log "image($image_name) has no newer version"
+    fi
+}
 ################################################################################################
 ensure_is_root
 
 install_docker
 
-# TODO: make docker is start
-# TODO: Docker start may fail, due to "There are no more loopback devices available."
-command="docker pull totvslabs/mdm:latest"
-log "run $command"
-output=$(eval $command)
-if ! echo "$output" | grep "Status: Image is up to date"; then
-    log "docker stop"
-    docker stop
+create_enough_loop_device
+
+if ! service docker status 1>/dev/null 2>/dev/null; then
+    service docker start
 fi
 
 log "prepare docker directory for couchbase"
 rm -rf /root/tmp/couchbase && mkdir -p /root/tmp/couchbase
 
-# log "docker start"
-# docker run -d -t --privileged --name mdm-jenkins -p 5022:22 -p 18000:18000 -p 18080:18080 totvslabs/mdm:latest /usr/sbin/sshd -D
-# docker run -d -t --privileged -v /root/tmp/couchbase/:/opt/couchbase/ --name mdm-all-in-one -p 8443:8443 -p 8091:8091 -p 9200:9200 -p 80:80 -p 6022:22 totvslabs/mdm:latest /usr/sbin/sshd -D
+# Start docker of mdm-jenkins
+image_name="totvslabs/mdm:latest"
+container_name="mdm-jenkins"
+docker_update_image $image_name $container_name
+container_status=$(is_container_running $container_name)
+if [ $container_status == "none" ]; then
+    docker run -d -t --privileged --name $container_name -p 5022:22 -p 18000:18000 -p 18080:18080 totvslabs/mdm:latest /usr/sbin/sshd -D
+elif [ $container_status == "dead" ]; then 
+    docker start $container_name    
+fi
 
-# # TODO: when docker start, make sure jenkins autostart
-# # Login to docker container need password
-# ssh -p 5022 root@127.0.0.1 service jenkins start
+# when docker start, make sure jenkins autostart
+docker exec mdm-jenkins service jenkins start
 
+# Start docker of mdm-all-in-one
+image_name="totvslabs/mdm:latest"
+container_name="mdm-all-in-one"
+docker_update_image $image_name $container_name
+container_status=$(is_container_running $container_name)
+if [ $container_status == "none" ]; then
+    docker run -d -t --privileged -v /root/tmp/couchbase/:/opt/couchbase/ --name $container_name -p 8443:8443 -p 8091:8091 -p 9200:9200 -p 80:80 -p 6022:22 totvslabs/mdm:latest /usr/sbin/sshd -D
+elif [ $container_status == "dead" ]; then 
+    docker start $container_name    
+fi
+
+log "Check docker containers: docker ps" 
+docker ps
+
+log "All set. Let's try Jenkins now: http://sandbox:18080"
 ## File : bootstrap_mdm_sandbox.sh ends
