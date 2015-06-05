cb468dd880f2ecd4a2c39a26f6af0860bd702d3c
filebat.mark@gmail.com <filebat.mark@gmail.com>
2015-06-04 22:37:15 -0400

    avoid apt-get update first

--- test/bootstrap_mdm_sandbox.sh
+++ test/bootstrap_mdm_sandbox.sh
@@ -5,7 +5,7 @@
 ## Description :
 ## --
 ## Created : <2015-05-28>
-## Updated: Time-stamp: <2015-06-04 21:13:23>
+## Updated: Time-stamp: <2015-06-04 22:36:59>
 ##-------------------------------------------------------------------
 function log() {
     local msg=${1?}
@@ -83,6 +83,10 @@ function is_container_running(){
 
 ################################################################################################
 ensure_is_root
+
+# Update env first
+rm /var/lib/apt/lists/* -vf
+# apt-get -y update # TODO: temporarily disable this, because it's too much traffic
 
 log "Install autostart script for /etc/init.d/mdm_sandbox"
 curl -o /etc/init.d/mdm_sandbox https://raw.githubusercontent.com/TOTVS/mdmpublic/master/test/mdm_sandbox.sh
