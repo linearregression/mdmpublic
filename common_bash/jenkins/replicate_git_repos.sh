d44fb6334d2a169e28c4a5ef8cbf4f4847b0d358
dennyzhang <denny.zhang001@gmail.com>
2016-04-15 17:28:31 +0800

    update scripts

---
+++ common_bash/jenkins/replicate_git_repos.sh
@@ -0,0 +1,110 @@
+#!/bin/bash -e
+##-------------------------------------------------------------------
+## @copyright 2016 DennyZhang.com
+## File : replicate_git_repos.sh
+## Author : DennyZhang.com <denny@dennyzhang.com>
+## Description :
+## --
+## Created : <2016-04-13>
+## Updated: Time-stamp: <2016-04-15 07:52:37>
+##-------------------------------------------------------------------
+
+################################################################################################
+## env variables:
+##      repo_list:
+##        git@github.com:TEST/test.git,master,git@gitlabcn.dennyzhang.com:customer/mdmdevops-test.git,master
+##        git@git.test.com:TEST/mytest.git,master,git@gitlabcn.dennyzhang.com:customer/mdmdevops-mytest.git,master
+##
+##       env_parameters:
+##             export CLEAN_START=false
+##             export working_dir=
+################################################################################################
+. /etc/profile
+################################################################################################
+if [ ! -f /var/lib/devops/refresh_common_library.sh ]; then
+    [ -d /var/lib/devops/ ] || (sudo mkdir -p  /var/lib/devops/ && sudo chmod 777 /var/lib/devops)
+    wget -O /var/lib/devops/refresh_common_library.sh \
+         https://raw.githubusercontent.com/DennyZhang/devops_public/master/common_library/refresh_common_library.sh
+fi
+# export AVOID_REFRESH_LIBRARY=true
+bash /var/lib/devops/refresh_common_library.sh "3372880711"
+. /var/lib/devops/devops_common_library.sh
+################################################################################################
+function shell_exit() {
+    errcode=$?
+    exit $errcode
+}
+
+function git_directory_commit() {
+    local code_dir=${1?}
+    local branch_name=${2?}
+    local commit_message=${3:-"Misc change"}
+
+    cd $code_dir
+    echo "Commit changes detected in intermediate directory"
+    git_status=$(git status)
+    if echo "$git_status" | grep "nothing to commit, working directory clean" 2>&1 1>/dev/null; then
+        echo "No change"
+    else
+        git add *
+        git commit -am $commit_message
+        git push origin $branch_name
+    fi
+}
+
+function replicate_git_repo() {
+    local git_repo_src_url=${1?}
+    local git_branch_src=${2?}
+    local git_repo_dst_url=${3?}
+    local git_branch_dst=${4?}
+    local working_dir=${5?}
+
+    [ -d $working_dir ] || mkdir -p $working_dir
+    local intermediate_dir="$working_dir/$git_branch_dst/intermediate"
+    local git_repo_src_name=$(echo ${git_repo_src_url%.git} | awk -F '/' '{print $2}')
+    local git_repo_dst_name=$(echo ${git_repo_dst_url%.git} | awk -F '/' '{print $2}')
+
+    echo "Update source git repo"
+    git_update_code $git_branch_src $working_dir $git_repo_src_url
+
+    echo "Update destination git repo"
+    git_update_code $git_branch_dst $working_dir $git_repo_dst_url
+
+    echo "Update intermediate directory: $intermediate_dir"
+    rm -rf $intermediate_dir && mkdir -p  $intermediate_dir
+    cp -r $working_dir/$git_branch_dst/$git_repo_dst_name/.git $intermediate_dir/
+    src_dir="$working_dir/$git_branch_src/$git_repo_src_name/"
+    for d in `ls -1a | grep -v "^.git$" | grep -v "^.$" | grep -v "^..$"`; do
+        cp -r $src_dir/$d  $intermediate_dir/
+    done
+
+    git_directory_commit $intermediate_dir $git_repo_dst_name
+}
+
+trap shell_exit SIGHUP SIGINT SIGTERM 0
+
+# Global variables needed to enable the current script
+env_parameters=$(remove_hardline "$env_parameters")
+env_parameters=$(list_strip_comments "$env_parameters")
+IFS=$'\n'
+for env_variable in `echo "$env_parameters"`; do
+    eval $env_variable
+done
+unset IFS
+
+########################################################
+[ -n "$working_dir" ] || working_dir="/var/lib/jenkins/code"
+[ -d "$working_dir" ] || mkdir -p $working_dir
+
+repo_list=$(list_strip_comments "$repo_list")
+for repo in `echo $repo_list`; do
+    repo=`echo $repo | sed 's/,/ /g'`
+    item=($repo)
+    git_repo_src=${item[0]}
+    git_branch_src=${item[1]}
+    git_repo_dst=${item[2]}
+    git_branch_dst=${item[3]}
+    echo "Replicate $git_repo_src:$git_branch_src to $git_repo_dst:$git_branch_dst"
+    replicate_git_repo $git_repo_src $git_branch_src $git_repo_dst $git_branch_dst $working_dir
+done
+## File : replicate_git_repos.sh ends
---
+++ common_bash/jenkins/system_cron_jobs.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+##-------------------------------------------------------------------
+## @copyright 2016 DennyZhang.com
+## File : system_cron_jobs.sh
+## Author : DennyZhang.com <denny@dennyzhang.com>
+## Description :
+## --
+## Created : <2016-04-13>
+## Updated: Time-stamp: <2016-04-15 09:17:17>
+##-------------------------------------------------------------------
+
+################################################################################################
+## env variables:
+##      cron_job_list:
+##        172.17.0.1:22:echo hello
+##        172.17.0.1:23:rm /tmp/npm-*
+##
+##       env_parameters:
+##         export ssh_key_file="/var/lib/jenkins/.ssh/id_rsa"
+################################################################################################
+. /etc/profile
+################################################################################################
+if [ ! -f /var/lib/devops/refresh_common_library.sh ]; then
+    [ -d /var/lib/devops/ ] || (sudo mkdir -p  /var/lib/devops/ && sudo chmod 777 /var/lib/devops)
+    wget -O /var/lib/devops/refresh_common_library.sh \
+         https://raw.githubusercontent.com/DennyZhang/devops_public/master/common_library/refresh_common_library.sh
+fi
+# export AVOID_REFRESH_LIBRARY=true
+bash /var/lib/devops/refresh_common_library.sh "3372880711"
+. /var/lib/devops/devops_common_library.sh
+################################################################################################
+
+# Global variables needed to enable the current script
+env_parameters=$(remove_hardline "$env_parameters")
+env_parameters=$(list_strip_comments "$env_parameters")
+IFS=$'\n'
+for env_variable in `echo "$env_parameters"`; do
+    eval $env_variable
+done
+unset IFS
+
+########################################################
+cron_job_list=$(list_strip_comments "$cron_job_list")
+
+[ -n "$ssh_key_file" ] || ssh_key_file="/var/lib/jenkins/.ssh/id_rsa"
+IFS=$'\n'
+for cron_job in ${cron_job_list[@]}
+do
+    unset IFS
+
+    IFS=:
+    item=($cron_job)
+    unset IFS
+
+    server_ip=${item[0]}
+    server_port=${item[1]}
+    string_prefix="$server_ip:$server_port:"
+    cron_command="${cron_job#${string_prefix}}"
+
+    ssh_connect="ssh -i $ssh_key_file -p $server_port -o StrictHostKeyChecking=no root@$server_ip"
+    echo "=============== $ssh_connect $cron_command"
+    $ssh_connect "$cron_command"
+done
+## File : system_cron_jobs.sh ends
