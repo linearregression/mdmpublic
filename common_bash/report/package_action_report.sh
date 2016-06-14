7f92641a71ee189079025a2dca7e1011e3b6db58
dennyzhang <denny@dennyzhang.com>
2016-06-14 08:50:38 +0800

    update scripts

--- common_bash/report/package_action_report.sh
+++ common_bash/report/package_action_report.sh
@@ -25,7 +25,7 @@ if [ ! -f /var/lib/devops/refresh_common_library.sh ]; then
     wget -O /var/lib/devops/refresh_common_library.sh \
          https://raw.githubusercontent.com/DennyZhang/devops_public/master/common_library/refresh_common_library.sh
 fi
-bash /var/lib/devops/refresh_common_library.sh "2361792088"
+bash /var/lib/devops/refresh_common_library.sh "1309341070"
 . /var/lib/devops/devops_common_library.sh
 ################################################################################################
 function generate_package_log_file() {
