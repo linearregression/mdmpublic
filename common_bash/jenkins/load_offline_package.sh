d1df87ee35511c24eaf5aae3cd664493e229dafd
dennyzhang <denny@dennyzhang.com>
2016-06-14 09:18:49 +0800

    update scripts

--- common_bash/jenkins/load_offline_package.sh
+++ common_bash/jenkins/load_offline_package.sh
@@ -27,7 +27,7 @@ if [ ! -f /var/lib/devops/refresh_common_library.sh ]; then
     wget -O /var/lib/devops/refresh_common_library.sh \
          https://raw.githubusercontent.com/DennyZhang/devops_public/master/common_library/refresh_common_library.sh
 fi
-bash /var/lib/devops/refresh_common_library.sh "1309341070"
+bash /var/lib/devops/refresh_common_library.sh "2727918430"
 . /var/lib/devops/devops_common_library.sh
 ################################################################################################
 function shell_exit() {
