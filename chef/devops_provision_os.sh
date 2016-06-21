fb5a400e5296bb73c514e98b72544ba7834e9dde
dennyzhang <denny@dennyzhang.com>
2016-06-21 10:37:18 +0800

    bug fixing

--- chef/devops_provision_os.sh
+++ chef/devops_provision_os.sh
@@ -7,9 +7,11 @@
 ## File : devops_provision_os.sh
 ## Author : Denny <denny@dennyzhang.com>
 ## Description :
+#   curl -o /root/devops_provision_os.sh https://raw.githubusercontent.com/TOTVS/mdmpublic/master/chef/devops_provision_os.sh
+#   bash -e /root/devops_provision_os.sh
 ## --
 ## Created : <2016-04-20>
-## Updated: Time-stamp: <2016-06-12 13:37:24>
+## Updated: Time-stamp: <2016-06-21 10:36:50>
 ##-------------------------------------------------------------------
 . /etc/profile
 
@@ -60,7 +62,7 @@ if [ -n "$git_deploy_key" ]; then
     cat > "$git_key_file" <<EOF
 $git_deploy_key
 EOF
-    chmod 400 git_key_file
+    chmod 400 "$git_key_file"
     cat > "/root/.ssh/config" <<EOF
 Host github.com
   StrictHostKeyChecking no
