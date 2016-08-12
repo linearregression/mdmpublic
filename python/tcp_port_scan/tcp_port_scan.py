f15acc9763670ce1c656478331f693fef966557c
dennyzhang <denny@dennyzhang.com>
2016-08-12 10:24:46 +0800

    update script

--- python/tcp_port_scan/tcp_port_scan.py
+++ python/tcp_port_scan/tcp_port_scan.py
@@ -7,7 +7,7 @@
 ## Description :
 ## --
 ## Created : <2016-01-15>
-## Updated: Time-stamp: <2016-08-12 09:39:40>
+## Updated: Time-stamp: <2016-08-12 10:24:04>
 ##-------------------------------------------------------------------
 import argparse
 import subprocess
@@ -47,7 +47,7 @@ def string_remove_patterns(string, opt_list):
     return "\n".join(l)
 
 def string_remove_extra_whitespace(string):
-    return ' '.join(string.split())
+    return ' '.join(string.split(' '))
 
 def load_paralist_from_file(fname):
     # TODO: defensive coding when file doesn't exist
@@ -79,10 +79,10 @@ def nmap_check(server_ip, ports):
 
 def get_portlist_by_nmap_output(nmap_output, server_ip):
     opt_list = ["Starting Nmap ", "Nmap scan report for ", "Host is ", \
-                "Not shown: ", " STATE ", " closed ports", \
-                " filtered unknown", "Nmap done: ", " scanned ports on "]
-    output = string_remove_patterns(nmap_output, opt_list)
-    output = string_remove_extra_whitespace(output)
+                "Not shown: ", " STATE ", " closed ports", " closed unknown"\
+                " filtered ", "Nmap done: ", " scanned ports on "]
+    output = string_remove_extra_whitespace(nmap_output)
+    output = string_remove_patterns(output, opt_list)
     output = strip_remove_emptylines(output)
     return output.split("\n")
 
